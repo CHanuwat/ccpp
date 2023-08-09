94b2fd888a5f444015f7260909c8c45a13ab0165
napakadon.lom <napakadon.lo@gmail.com>
2023-08-09 21:17:25 +0700

    [FIX] Dashboard Approve

--- models/approve_activity_type.py
+++ models/approve_activity_type.py
@@ -18,12 +18,41 @@ class ApproveActivityType(models.Model):
     code = fields.Char("Code")
     color = fields.Integer(string="Color")
     count_doc = fields.Integer(string="Count Document", compute="_compute_count_doc",store=False)
+    icon_variant =  fields.Image("Icon Variant Image", max_width=1920, max_height=1920)
+    icon = fields.Image("Icon Image", compute='_compute_image', inverse='_set_image')
+    icon_binary = fields.Binary("Icon", attachment=True,copy=False)
+    info = fields.Text("Info")
+    #background_color = fields.Char("Background Color", default="#ffffff")
+
+    def _compute_image(self):
+        """Get the image from the template if no image is set on the variant."""
+        for record in self:
+            record.icon = record.icon_variant
+
+    def _set_image(self):
+        return self._set_template_field('icon', 'icon_variant')
+    
+    def _set_template_field(self, template_field, variant_field):
+        for record in self:
+            if (
+                # We are trying to remove a field from the variant even though it is already
+                # not set on the variant, remove it from the template instead.
+                (not record[template_field] and not record[variant_field])
+                # We are trying to add a field to the variant, but the template field is
+                # not set, write on the template instead.
+                #or (record[template_field] and not record.product_tmpl_id[template_field])
+                # There is only one variant, always write on the template.
+            ):
+                record[variant_field] = False
+                #record.product_tmpl_id[template_field] = record[template_field]
+            else:
+                record[variant_field] = record[template_field]
     
     def _compute_count_doc(self):
         for obj in self:
             count_doc = 0
             employee_id = self.env['hr.employee'].search([('user_id','=',self.env.user.id)],limit=1)
-            activity_ids = self.env['mail.activity'].search([('job_id','in',employee_id.job_lines.ids),('approve_activity_type_id','=',obj.id)])
+            activity_ids = self.env['mail.activity'].search([('job_id','=',employee_id.job_id.id),('approve_activity_type_id','=',obj.id)])
             count_doc = len(activity_ids)
             obj.count_doc = count_doc
         
@@ -31,7 +60,7 @@ class ApproveActivityType(models.Model):
         for obj in self:
             action = True
             employee_id = self.env['hr.employee'].search([('user_id','=',self.env.user.id)],limit=1)
-            activity_ids = self.env['mail.activity'].search([('job_id','in',employee_id.job_lines.ids),('approve_activity_type_id','=',obj.id)])
+            activity_ids = self.env['mail.activity'].search([('job_id','=',employee_id.job_id.id),('approve_activity_type_id','=',obj.id)])
             record_ids = activity_ids.mapped('res_id')
             if obj.code == 'ccpp':
                 action = self.env['ir.actions.act_window']._for_xml_id('ccpp.ccpp_ccpp_approve_dashboard_action')
